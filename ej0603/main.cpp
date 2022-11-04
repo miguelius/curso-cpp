9dcfd504af4b9b4cc55f7fc341cdfddbd9a2c11e
Miguel <mcarboni@afip.gob.ar>
2022-11-04 20:28:25 -0300

    ejecicios 6

---
+++ ej0603/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int pedirOpcion(const string mensaje, const int max = 99) {
+  auto x = 0;
+  cout << mensaje << "(1 - " << max << ") "<< ": ";
+  cin >> x;
+  if (0 > x || x > max) {
+    return -1;
+  }
+  return x;
+}
+
+double pedirDecimal(const string mensaje) {
+  auto x = 0.;
+  cout << mensaje << ": ";
+  cin >> x;
+  return x;
+}
+
+void imprimirMenu() {
+  cout << "Qué hacemos con los numeritos que cargaste?" << endl;
+  cout << "1) +" << endl;
+  cout << "2) -" << endl;
+  cout << "3) *" << endl;
+  cout << "4) /" << endl;
+
+}
+
+double sum(const double x, const double y) noexcept { return x + y; }
+double sub(const double x, const double y) noexcept { return x - y; }
+double mul(const double x, const double y) noexcept { return x * y; }
+double div(const double x, const double y) { return x / y; }
+
+int main() {
+  double (*operaciones[])(const double, const double) = { sum, sub, mul, div};
+  auto x = pedirDecimal("Ingresar un número");
+  auto y = pedirDecimal("Ingresar otro número");
+  imprimirMenu();
+  auto opcion = pedirOpcion("Ingrese una opción ", 4);
+  if (opcion == -1) {
+    cout << "Así no se puede! Ingresaste una opción inválida." << endl;
+    return -1;
+  }
+  cout << "El resultado es " << (operaciones[opcion - 1])(x, y) << endl;
+  return 0;
+}
+
