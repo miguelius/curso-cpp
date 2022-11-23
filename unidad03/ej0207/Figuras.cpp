da5c2cc48a382169755114da5603816825f794bc
Miguel <mcarboni@afip.gob.ar>
2022-11-23 00:15:29 -0300

    ejercicio 7. superclase con discriminador por tipo.

---
+++ unidad03/ej0207/Figuras.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include "Figuras.hpp"
+using std::max;
+using std::cerr;
+using std::endl;
+
+string Figura::Tipo() const {
+  return this->_tipo;
+}
+
+string Figura::Color() const {
+  return this->_color;
+}
+
+void Figura::Color(const string color) {
+  this->_color = color;
+}
+
+int Figura::Lados() const { 
+  if ("Generico" != this->Tipo() && "Cuadrado" != this->Tipo() && "Circulo" != this->Tipo()) {
+    cerr << "Tipo: " << this->Tipo() << " desconocido. " << endl;
+    return -1;
+  } 
+  return this->_lados;
+}
+
+float Figura::Area() const { 
+  if ("Generico" == this->Tipo()) {
+    return 0;
+  } if ("Cuadrado" == this->Tipo()) {
+    auto l = ((Cuadrado *)this)->Lado();
+    return l*l;
+  } if ("Circulo" == this->Tipo()) {
+    auto r = ((Circulo *)this)->Radio();
+    return r * r * 3.14;
+  } 
+  cerr << "Tipo: " << this->Tipo() << " desconocido. " << endl;
+  return -1;
+}
+
+float Figura::Perimetro() const { 
+  if ("Generico" == this->Tipo()) {
+    return 0;
+  } if ("Cuadrado" == this->Tipo()) {
+    auto l = ((Cuadrado *)this)->Lado();
+    return 4*l;
+  } if ("Circulo" == this->Tipo()) {
+    auto d = ((Circulo *)this)->Diametro();
+    return d * 3.14;
+  } 
+  cerr << "Tipo: " << this->Tipo() << " desconocido. " << endl;
+  return -1;
+}
+
+Generico::Generico(const int lados) {
+  this->_tipo = "Generico";
+  this->_lados = lados;
+}
+
+Circulo::Circulo(const float radio) {
+  this->_tipo = "Circulo";
+  this->_radio = radio;
+  this->_lados = 0;
+}
+
+float Circulo::Radio() const {
+  return this->_radio;
+}
+
+float Circulo::Diametro() const {
+  return this->_radio * 2;
+}
+
+Cuadrado::Cuadrado(const float lado) {
+  this->_tipo = "Cuadrado";
+  this->_lado = lado;
+  this->_lados = 4;
+}
+
+float Cuadrado::Lado() const {
+  return this->_lado;
+}
---
+++ unidad03/ej0207/Figuras.hpp
@@ -0,0 +1,54 @@
+#ifndef FIGURAS_HPP
+#define FIGURAS_HPP
+#include <string>
+using std::string;
+
+class Figura {
+  protected:
+    string _tipo;
+    string _color;
+    int _lados;
+
+  public:
+    float Area() const;
+    float Perimetro() const;
+
+    string Tipo() const;
+    int Lados() const;
+
+    // accesors
+    string Color() const;
+    void Color(const string color);
+};
+
+class Generico: public Figura {
+  
+  public:
+    Generico(const int lados);
+    
+};
+
+class Cuadrado: public Figura {
+  private:
+    float _lado;
+  
+  public:
+    Cuadrado(const float lado);
+    
+    float Lado() const;
+};
+
+class Circulo: public Figura {
+  private:
+    float _radio;
+  
+  public:
+    Circulo(const float r);
+    float Diametro() const;
+
+    float Radio() const;
+    void Radio(const float r);
+
+};
+
+#endif
